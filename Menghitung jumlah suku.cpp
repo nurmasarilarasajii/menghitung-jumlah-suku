2f27b67cc65b1373eb16cf00934a96c726264e9c
nurmasarilarasajii <72746534+nurmasarilarasajii@users.noreply.github.com>
2020-10-28 12:52:12 +0700

    Add files via upload

---
+++ Menghitung jumlah suku.cpp
@@ -0,0 +1,23 @@
+/* Menghitung Jumlah Suku */
+/* Nurma Sari Laras Aji */
+/* 20051397062 */
+/* MI/2020B */
+
+#include <conio.h>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+	int main(){
+int i,b;	
+for(i=1;i<7;i++){
+	 b = pow(i, 3);
+	 
+	 
+	 cout<<b<<",";
+	 	
+	}
+	return 0;
+}
